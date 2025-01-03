3b7adf162237fe0c8846ae574741d47e3463d6a1
John <deedsoftheforsaken@gmail.com>
2025-01-02 21:33:30 -0500

    Added funcs `BlackBg` and `BlackBgf` Added funcs `RedBg` and `RedBgf` Added funcs `GreenBg` and `GreenBgf` Added funcs `YellowBg` and `YellowBgf` Added funcs `BlueBg` and `BlueBgf` Added funcs `MagentaBg` and `MagentaBgf` Added funcs `CyanBg` and `CyanBgf` Added funcs `WhiteBg` and `WhiteBgf`

--- prettyprint.go
+++ prettyprint.go
@@ -7,13 +7,13 @@ import (
 )
 
 var (
-	// IconComplete is the icon for a completed task.
+	// IconComplete is the icon for a completed message.
 	IconComplete = "[✓] "
 
-	// IconAlert is the icon for an alert.
+	// IconAlert is the icon for an alerted message.
 	IconAlert = "[!] "
 
-	// IconFailed is the icon for a failed task.
+	// IconFailed is the icon for a failed message.
 	IconFailed = "[✗] "
 
 	// IconInfo is the icon for an informational message.
@@ -33,6 +33,19 @@ func Blackf(format string, a ...any) string {
 	return ansi.Black + fmt.Sprintf(format, a...) + ansi.Reset
 }
 
+// BlackBg formats using the default formats for its operands and returns the
+// resulting string with a black background. Spaces are added between
+// operands when neither is a string.
+func BlackBg(a ...any) string {
+	return ansi.BlackBg + fmt.Sprint(a...) + ansi.Reset
+}
+
+// BlackBgf formats according to a format specifier and returns the resulting
+// string with a black background.
+func BlackBgf(format string, a ...any) string {
+	return ansi.BlackBg + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
 // Red formats using the default formats for its operands and returns the
 // resulting string with a red foreground. Spaces are added between
 // operands when neither is a string.
@@ -44,6 +57,19 @@ func Red(a ...any) string {
 // string with a red foreground.
 func Redf(format string, a ...any) string {
 	return ansi.Red + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
+// RedBg formats using the default formats for its operands and returns the
+// resulting string with a red background. Spaces are added between
+// operands when neither is a string.
+func RedBg(a ...any) string {
+	return ansi.RedBg + fmt.Sprint(a...) + ansi.Reset
+}
+
+// RedBgf formats according to a format specifier and returns the resulting
+// string with a red background.
+func RedBgf(format string, a ...any) string {
+	return ansi.RedBg + fmt.Sprintf(format, a...) + ansi.Reset
 }
 
 // Green formats using the default formats for its operands and returns the
@@ -59,6 +85,19 @@ func Greenf(format string, a ...any) string {
 	return ansi.Green + fmt.Sprintf(format, a...) + ansi.Reset
 }
 
+// GreenBg formats using the default formats for its operands and returns the
+// resulting string with a green background. Spaces are added between
+// operands when neither is a string.
+func GreenBg(a ...any) string {
+	return ansi.GreenBg + fmt.Sprint(a...) + ansi.Reset
+}
+
+// GreenBgf formats according to a format specifier and returns the resulting
+// string with a green background.
+func GreenBgf(format string, a ...any) string {
+	return ansi.GreenBg + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
 // Yellow formats using the default formats for its operands and returns the
 // resulting string with a yellow foreground. Spaces are added between
 // operands when neither is a string.
@@ -70,6 +109,19 @@ func Yellow(a ...any) string {
 // string with a yellow foreground.
 func Yellowf(format string, a ...any) string {
 	return ansi.Yellow + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
+// YellowBg formats using the default formats for its operands and returns the
+// resulting string with a yellow background. Spaces are added between
+// operands when neither is a string.
+func YellowBg(a ...any) string {
+	return ansi.YellowBg + fmt.Sprint(a...) + ansi.Reset
+}
+
+// YellowBgf formats according to a format specifier and returns the resulting
+// string with a yellow background.
+func YellowBgf(format string, a ...any) string {
+	return ansi.YellowBg + fmt.Sprintf(format, a...) + ansi.Reset
 }
 
 // Blue formats using the default formats for its operands and returns the
@@ -85,6 +137,19 @@ func Bluef(format string, a ...any) string {
 	return ansi.Blue + fmt.Sprintf(format, a...) + ansi.Reset
 }
 
+// BlueBg formats using the default formats for its operands and returns the
+// resulting string with a blue background. Spaces are added between
+// operands when neither is a string.
+func BlueBg(a ...any) string {
+	return ansi.BlueBg + fmt.Sprint(a...) + ansi.Reset
+}
+
+// BlueBgf formats according to a format specifier and returns the resulting
+// string with a blue background.
+func BlueBgf(format string, a ...any) string {
+	return ansi.BlueBg + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
 // Magenta formats using the default formats for its operands and returns the
 // resulting string with a magenta foreground. Spaces are added between
 // operands when neither is a string.
@@ -96,6 +161,19 @@ func Magenta(a ...any) string {
 // string with a magenta foreground.
 func Magentaf(format string, a ...any) string {
 	return ansi.Magenta + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
+// MagentaBg formats using the default formats for its operands and returns the
+// resulting string with a magenta background. Spaces are added between
+// operands when neither is a string.
+func MagentaBg(a ...any) string {
+	return ansi.MagentaBg + fmt.Sprint(a...) + ansi.Reset
+}
+
+// MagentaBgf formats according to a format specifier and returns the resulting
+// string with a magenta background.
+func MagentaBgf(format string, a ...any) string {
+	return ansi.MagentaBg + fmt.Sprintf(format, a...) + ansi.Reset
 }
 
 // Cyan formats using the default formats for its operands and returns the
@@ -111,6 +189,19 @@ func Cyanf(format string, a ...any) string {
 	return ansi.Cyan + fmt.Sprintf(format, a...) + ansi.Reset
 }
 
+// CyanBg formats using the default formats for its operands and returns the
+// resulting string with a cyan background. Spaces are added between
+// operands when neither is a string.
+func CyanBg(a ...any) string {
+	return ansi.CyanBg + fmt.Sprint(a...) + ansi.Reset
+}
+
+// CyanBgf formats according to a format specifier and returns the resulting
+// string with a cyan background.
+func CyanBgf(format string, a ...any) string {
+	return ansi.CyanBg + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
 // White formats using the default formats for its operands and returns the
 // resulting string with a white foreground. Spaces are added between
 // operands when neither is a string.
@@ -122,6 +213,19 @@ func White(a ...any) string {
 // string with a white foreground.
 func Whitef(format string, a ...any) string {
 	return ansi.White + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
+// WhiteBg formats using the default formats for its operands and returns the
+// resulting string with a white background. Spaces are added between
+// operands when neither is a string.
+func WhiteBg(a ...any) string {
+	return ansi.WhiteBg + fmt.Sprint(a...) + ansi.Reset
+}
+
+// WhiteBgf formats according to a format specifier and returns the resulting
+// string with a white background.
+func WhiteBgf(format string, a ...any) string {
+	return ansi.WhiteBg + fmt.Sprintf(format, a...) + ansi.Reset
 }
 
 // Complete returns a string with a green checkmark icon and the given message.
