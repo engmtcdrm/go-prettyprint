8ced849af0077669943dc628317086f56c4c10fb
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-01-08 12:59:35 -0500

    Added Fg8Bit Added Fg8Bitf Added Fg24Bit Added Fg24Bitf

--- fg.go
+++ fg.go
@@ -109,3 +109,61 @@ func White(a ...any) string {
 func Whitef(format string, a ...any) string {
 	return ansi.White + fmt.Sprintf(format, a...) + ansi.Reset
 }
+
+// Fg8Bit formats using the default formats for its operands and returns the
+// resulting string with an 8-bit foreground color. Spaces are added between
+// operands when neither is a string.
+//
+// color must be between 0 and 255 otherwise no color will be applied.
+func Fg8Bit(color int, a ...any) string {
+	fgColor := ansi.Foreground8Bit(color)
+
+	if fgColor == "" {
+		return fmt.Sprint(a...)
+	}
+
+	return fgColor + fmt.Sprint(a...) + ansi.Reset
+}
+
+// Fg8Bitf formats according to a format specifier and returns the resulting
+// string with an 8-bit foreground color.
+//
+// color must be between 0 and 255 otherwise no color will be applied.
+func Fg8Bitf(color int, format string, a ...any) string {
+	fgColor := ansi.Foreground8Bit(color)
+
+	if fgColor == "" {
+		return fmt.Sprintf(format, a...)
+	}
+
+	return fgColor + fmt.Sprintf(format, a...) + ansi.Reset
+}
+
+// Fg24Bit formats using the default formats for its operands and returns the
+// resulting string with an 24-bit foreground color. Spaces are added between
+// operands when neither is a string.
+//
+// r, g, and b must be between 0 and 255 otherwise no color will be applied.
+func Fg24Bit(r int, g int, b int, a ...any) string {
+	fgColor := ansi.Foreground24Bit(r, g, b)
+
+	if fgColor == "" {
+		return fmt.Sprint(a...)
+	}
+
+	return fgColor + fmt.Sprint(a...) + ansi.Reset
+}
+
+// Fg24Bitf formats according to a format specifier and returns the resulting
+// string with an 24-bit foreground color.
+//
+// r, g, and b must be between 0 and 255 otherwise no color will be applied.
+func Fg24Bitf(r int, g int, b int, format string, a ...any) string {
+	fgColor := ansi.Foreground24Bit(r, g, b)
+
+	if fgColor == "" {
+		return fmt.Sprintf(format, a...)
+	}
+
+	return fgColor + fmt.Sprintf(format, a...) + ansi.Reset
+}
