0b3f82cfb749b69c9f931ca19c309f55422fe58c
John <deedsoftheforsaken@gmail.com>
2025-01-08 20:26:00 -0500

    Refactored to Print8BitColors func Speed up performance Added determineColorSlice func Added Print24BitColors func

--- examples/examples.go
+++ examples/examples.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
 	pp "github.com/engmtcdrm/go-prettyprint"
 )
@@ -20,64 +21,122 @@ func centerStr(s string, width int) string {
 	return fmt.Sprint(strings.Repeat(" ", (width-len(s))/2), s, strings.Repeat(" ", width))[0:width]
 }
 
-func main() {
+func Print8BitColors() {
 	padding := 8
 
-	fmt.Print("Standard: ")
+	start := time.Now()
+
+	cStr := "Standard: "
 
 	for i := 0; i < 8; i++ {
-		fmt.Print(pp.Bg8Bit(i, centerStr(strconv.Itoa(i), padding)))
+		cStr += pp.Bg8Bit(i, centerStr(strconv.Itoa(i), padding))
 	}
 
-	fmt.Println()
-	fmt.Print("Intense:  ")
+	cStr += "\nIntense:  "
 
 	for i := 8; i < 16; i++ {
-		fmt.Print(pp.Bg8Bit(i, centerStr(strconv.Itoa(i), padding)))
+		cStr += pp.Bg8Bit(i, centerStr(strconv.Itoa(i), padding))
 	}
 
-	fmt.Println()
-	fmt.Println()
+	cStr += "\n\n"
 
 	d := 0
 	d2 := 0
 
 	for i := 0; i < 160; i += 72 {
 		for j := 16 + i; j < 16+i+36; j++ {
-			fmt.Print(pp.Bg8Bit(j, centerStr(strconv.Itoa(j), padding)))
+			cStr += pp.Bg8Bit(j, centerStr(strconv.Itoa(j), padding))
 
 			d++
 
 			if d == 6 {
-				fmt.Print("  ")
+				cStr += "  "
+				// fmt.Print("  ")
 				for k := 31; k < 37; k++ {
-					fmt.Print(pp.Bg8Bit(j+k, centerStr(strconv.Itoa(j+k), padding)))
+					cStr += pp.Bg8Bit(j+k, centerStr(strconv.Itoa(j+k), padding))
 				}
 
-				fmt.Println()
+				cStr += "\n"
 				d = 0
 				d2++
 			}
 
 			if d2 == 6 {
-				fmt.Println()
+				cStr += "\n"
 				d2 = 0
 			}
 		}
 	}
 
-	fmt.Print("Greyscale: ")
+	cStr += "Greyscale: "
 
 	for i := 232; i < 244; i++ {
-		fmt.Print(pp.Bg8Bit(i, centerStr(strconv.Itoa(i), padding)))
+		cStr += pp.Bg8Bit(i, centerStr(strconv.Itoa(i), padding))
 	}
 
-	fmt.Println()
-	fmt.Print(strings.Repeat(" ", 11))
+	cStr += "\n           "
 
 	for i := 244; i < 256; i++ {
-		fmt.Print(pp.Bg8Bit(i, centerStr(strconv.Itoa(i), padding)))
+		cStr += pp.Bg8Bit(i, centerStr(strconv.Itoa(i), padding))
 	}
 
+	fmt.Println(cStr)
 	fmt.Println()
+
+	end := time.Now()
+
+	fmt.Println("Time taken:", end.Sub(start))
+}
+
+func determineColorSlice(n int) []int {
+	if n < 0 || n > 255 {
+		s := []int{}
+
+		for i := 0; i < 256; i++ {
+			s = append(s, i)
+		}
+
+		return s
+	}
+
+	return []int{n}
+}
+
+func Print24BitColors(r int, g int, b int) {
+	rSlice := determineColorSlice(r)
+	gSlice := determineColorSlice(g)
+	bSlice := determineColorSlice(b)
+
+	d := 0
+	cStr := ""
+
+	start := time.Now()
+
+	for _, r := range rSlice {
+		for _, g := range gSlice {
+			for _, b := range bSlice {
+				cStr += pp.Bg24Bit(r, g, b, centerStr(fmt.Sprintf("%d,%d,%d", r, g, b), 13))
+
+				d++
+
+				if d == 8 {
+					cStr += "\n"
+					d = 0
+				}
+			}
+
+			cStr += "\n"
+			fmt.Print(cStr)
+			cStr = ""
+		}
+	}
+
+	end := time.Now()
+
+	fmt.Println("Time taken:", end.Sub(start))
+}
+
+func main() {
+	Print8BitColors()
+	Print24BitColors(-1, -1, -1)
 }
